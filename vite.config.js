960521b15c58594fda6719a44efa0b5db38e7e2a
Harsimran Kaur <harsimrankar19@gmail.com>
2024-04-02 19:29:12 +0530

    okay

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+// export defineConfig({
+//   // Specify CSS file handling options
+//   css: {
+//     // Extract CSS into separate files (set to `false` to inline CSS into JS files)
+//     extract: true,
+//   },
+// });
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  // Specify Vite configuration options
   plugins: [react()],
-})
+  css: {
+    // Specify CSS file handling options
+    extract: true, // Extract CSS into separate files (set to `false` to inline CSS into JS files)
+  },
+});
