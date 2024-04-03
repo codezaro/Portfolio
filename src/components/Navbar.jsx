0e656a7d5f117162136db291c19f1282da463044
Harsimran Kaur <harsimrankar19@gmail.com>
2024-04-03 22:55:42 +0530

    update image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export const Navbar = ({
         <div>
           <img
             className="sm:h-[150px] sm:w-[150px]  h-[80px] w-[80px]"
-            src="/public/Images/avatar.png"
+            src="./Portfolio/public/Images/avatar.png"
             alt=""
           />
         </div>
