b981816266f4f9732527e814049791f5184493ff
Harsimran Kaur <harsimrankar19@gmail.com>
2024-04-06 12:47:00 +0530

    Revert "r"
    This reverts commit c9427f286afb96bafd701dee13bc861128b23f9a.

--- src/components/Mea.jsx
+++ src/components/Mea.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Mea = () => {
   return (
     <div className="flex gap-5 pt-[250px] px-10 ml-6 flex-col">
-      <p className="font-medium text-xl md:text-lg">
+      <p className="font-medium text-lg">
         As a frontend developer,I bring a unique blend of technical expertise to
         the table with a passion for coding and a strong foundation in CSS,
         HTML,JavaScript, ReactJS, and proficiency in handling Git and Github.
