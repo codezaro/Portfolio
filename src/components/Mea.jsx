ad4bf8f31f1cb06127b4b8d04a96620fb031e70c
harsimran2 <harsimran@circlechess.com>
2026-04-30 14:35:57 +0530

    updated formatting

--- src/components/Mea.jsx
+++ src/components/Mea.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React from 'react';
 
 export const Mea = () => {
   return (
-    <div className="flex gap-5 pt-[250px] px-10 ml-6 flex-col">
-      <p className="font-medium text-lg">
-         As a frontend developer,I bring a unique blend of technical expertise to
-        the table with a passion for coding.
-       At CircleChess, I worked extensively with ReactJS, React Native, NextJS, and other modern web technologies to create intuitive and high-performance interfaces for a real-time gaming environment and developing mobile applications compatible for Android and iOS leveraging tools like XCode, Android Studio. My role involved optimizing application performance, managing complex state using Redux, Zustand, and Context API, and ensuring seamless cross-platform compatibility. These experiences enhanced my ability to deliver high-quality user experiences making the use of AI tools like Claude and Cursor and address the unique challenges of interactive, real-time applications.
-My technical expertise includes integrating RESTful APIs, implementing smooth animations, and using tools like Tailwind CSS to craft visually appealing and responsive designs. Proficient in version control systems like Git, GitHub, and Bitbucket, I collaborated effectively within the team, using tools like Figma for design.
-Additionally, I am familiar with technologies like WebSockets, Socket.IO, SASS, Bootstrap, and Sentry, which have expanded my ability to debug, monitor, and enhance application functionality. I thrive in fast-paced environments where innovation and user-focused design are at the forefront.
+    <div className="ml-6 flex flex-col gap-5 px-10 pt-[250px]">
+      <p className="text-lg font-medium">
+        As a frontend developer,I bring a unique blend of technical expertise to
+        the table with a passion for coding. At CircleChess(a chess coaching and
+        gaming startup), I worked extensively with ReactJS, React Native,
+        NextJS, and other modern web technologies to create intuitive and
+        high-performance interfaces for a real-time gaming environment and
+        developing mobile applications compatible for Android and iOS leveraging
+        tools like XCode, Android Studio. My role involved optimizing
+        application performance, managing complex state using Redux, Zustand,
+        and Context API, and ensuring seamless cross-platform compatibility.
+        These experiences enhanced my ability to deliver high-quality user
+        experiences making the use of AI tools like Claude and Cursor and
+        address the unique challenges of interactive, real-time applications. My
+        technical expertise includes integrating RESTful APIs, implementing
+        smooth animations, and using tools like Tailwind CSS to craft visually
+        appealing and responsive designs. Proficient in version control systems
+        like Git, GitHub, and Bitbucket, I collaborated effectively within the
+        team, using tools like Figma for design. Additionally, I am familiar
+        with technologies like WebSockets, Socket.IO, SASS, Bootstrap, and
+        Sentry, which have expanded my ability to debug, monitor, and enhance
+        application functionality. I thrive in fast-paced environments where
+        innovation and user-focused design are at the forefront.
         <br /> I am constantly seeking opportunities to enhance my adroitness
         and stay updated on industry trends. I am confident that my combination
         of technical skills, and commitment to growth make me a valuable asset
